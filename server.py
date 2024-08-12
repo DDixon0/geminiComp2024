f7b5be51d188ed29993a89141ca16ec7e1709f4a
DDixon0 <dixon.denzell@gmail.com>
2024-08-12 00:38:00 -0400

    Pretty much done, adding aria-labels, cleaned up code and comments,  added expert opinions.

--- server.py
+++ server.py
@@ -26,22 +26,6 @@ def get_Chat_response(text):
     out = re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', out)
     # print(out)
     return out
-# @app.route('/sigma')
-# def sigma():
-#     return render_template('sigma.html')
-
-
-#Handaling a route for requests 
-# @app.route('/page')
-# def get_data():
-#     info = request.args.get('_____')
-#     response_data = user_query_gemini(info)
-#     handle the case where the querey response was not what we expected
-
-#     return render_template(
-#         "site.html",
-#         item1=response_data['one'],
-#         item2=response_data['two']['second'])
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000) # Need Waitess to serve the file/ server
--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,109 +1,3 @@
-// //links
-// //http://eloquentjavascript.net/09_regexp.html
-// //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-
-
-// var messages = [], //array that hold the record of each string in chat
-//   lastUserMessage = "", //keeps track of the most recent input string from the user
-//   botMessage = "", //var keeps track of what the chatbot is going to say
-//   botName = 'Chatbot', //name of the chatbot
-//   talking = true; //when false the speach function doesn't work
-// //
-// //
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //edit this function to change what the chatbot says
-// function chatbotResponse() {
-//   talking = true;
-//   botMessage = "I'm confused"; //the default message
-
-//   if (lastUserMessage === 'hi' || lastUserMessage =='hello') {
-//     const hi = ['hi','howdy','hello']
-//     botMessage = hi[Math.floor(Math.random()*(hi.length))];;
-//   }
-
-//   if (lastUserMessage === 'name') {
-//     botMessage = 'My name is ' + botName;
-//   }
-// }
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //****************************************************************
-// //
-// //
-// //
-// //this runs each time enter is pressed.
-// //It controls the overall input and output
-// function newEntry() {
-//   //if the message from the user isn't empty then run 
-//   if (document.getElementById("chatbox").value != "") {
-//     //pulls the value from the chatbox ands sets it to lastUserMessage
-//     lastUserMessage = document.getElementById("chatbox").value;
-//     //sets the chat box to be clear
-//     document.getElementById("chatbox").value = "";
-//     //adds the value of the chatbox to the array messages
-//     messages.push(lastUserMessage);
-//     //Speech(lastUserMessage);  //says what the user typed outloud
-//     //sets the variable botMessage in response to lastUserMessage
-//     chatbotResponse();
-//     //add the chatbot's name and message to the array messages
-//     messages.push("<b>" + botName + ":</b> " + botMessage);
-//     // says the message using the text to speech function written below
-//     Speech(botMessage);
-//     //outputs the last few array elements of messages to html
-//     for (var i = 1; i < 18; i++) {
-//       if (messages[messages.length - i])
-//         document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
-//     }
-//   }
-// }
-
-// //text to Speech
-// //https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
-// function Speech(say) {
-//   if ('speechSynthesis' in window && talking) {
-//     var utterance = new SpeechSynthesisUtterance(say);
-//     //msg.voice = voices[10]; // Note: some voices don't support altering params
-//     //msg.voiceURI = 'native';
-//     //utterance.volume = 1; // 0 to 1
-//     //utterance.rate = 0.1; // 0.1 to 10
-//     //utterance.pitch = 1; //0 to 2
-//     //utterance.text = 'Hello World';
-//     //utterance.lang = 'en-US';
-//     speechSynthesis.speak(utterance);
-//   }
-// }
-
-// //runs the keypress() function when a key is pressed
-// document.onkeypress = keyPress;
-// //if the key pressed is 'enter' runs the function newEntry()
-// function keyPress(e) {
-//   var x = e || window.event;
-//   var key = (x.keyCode || x.which);
-//   if (key == 13 || key == 3) {
-//     //runs this function when enter is pressed
-//     newEntry();
-//   }
-//   if (key == 38) {
-//     console.log('hi')
-//       //document.getElementById("chatbox").value = lastUserMessage;
-//   }
-// }
-// //clears the placeholder text ion the chatbox
-// //this function is set to run when the users brings focus to the chatbox, by clicking on it
-// function placeHolder() {
-//   document.getElementById("chatbox").placeholder = "";
-// }
-
 
 //CHAT
 
@@ -126,7 +20,7 @@ $("#messageArea").on("submit", function(event) {
     type: "POST",
     url: "/get",
   }).done(function(data) {
-    var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
+    var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="/static/assets/GG.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
     $("#messageFormeight").append($.parseHTML(botHtml));
   });
   event.preventDefault();
@@ -153,6 +47,74 @@ chat.addEventListener("mouseover", () =>{
   }
 })
 
+
+//Side Experts
+
+function Localize(){
+  let query = "If possible, regenerate another set of cause and effects. However, this time, have a focus on the local impacts and variables. Local causal variables are immediate and can quickly be observed. For this response only, use friendly language, like a neighbor or friend."
+  expertRequest("Local",query);
+}
+
+function Municipalize(){
+  let query = "If possible, regenerate another set of cause and effects. However, this time, have a focus on the Municipal impacts and variables. Municiapl causal variables are not as noticeable but affect the larger community. For this response only, use teacherly language like a teacher or principal."
+  expertRequest("Municipal",query);
+}
+
+function Nationalize(){
+  let query = "If possible, regenerate another set of cause and effects. However, this time, have a focus on the National impacts and variables. National causal variables are a result of compounding multiple fo the same event. This affects the entire country. For this response only, use political language like a politician or governor."
+  expertRequest("National",query);
+}
+
+function Globalize(){
+  let query = "If possible, regenerate another set of cause and effects. However, this time, have a focus on the Global impacts and variables. Global causal variables are geopolitical in context and are a result of competing elements. This affects the entire world. For this response only, use powerful language like a PHD expert or a President."
+  expertRequest("Global",query);
+  
+}
+
+function Economize(){
+  let query = "If possible, regenerate another set of cause and effects. However, this time, have a focus on the Economical impacts and variables. Economical causal variables are about the stock market, access to resources, the performacne of comapnies and cash flow. Economical impacts can be as small as local business or large as international conglomerates. For this response only, use investor language like an investment banker or a salesman."
+  expertRequest("Economical",query);
+  
+}
+
+function Ecologize(){
+  let query = "If possible, regenerate another set of cause and effects. However, this time, have a focus on the Ecological impacts and variables. Ecological causal variables are about the local enviroment, global enviroment, biosphere, small and big organisim. Ecological impacts are complex and can occur immediately or over hundres of years through evolution. For this response only, use warm-stoic language like an enviromental engineer."
+  expertRequest("Ecological",query);
+}
+
+
+// Specified Expert Request, Council of ELders
+function expertRequest(expert, prompt){
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const str_time = hour+":"+minute;
+  let rawText = prompt;
+
+  var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="/static/assets/GG_'+ expert +'.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer"> Attempting to Find the ' + expert + ' impact.<span class="msg_time">' + str_time + '</span></div></div>';
+
+  $("#messageFormeight").append($.parseHTML(botHtml));
+
+  $.ajax({
+    data: {
+      msg: rawText,	
+    },
+    type: "POST",
+    url: "/get",
+  }).done(function(data) {
+    var botHtml = '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="/static/assets/GG_'+ expert +'.png" class="rounded-circle user_img_msg"></div><div class="msg_cotainer">' + data + '<span class="msg_time">' + str_time + '</span></div></div>';
+    $("#messageFormeight").append($.parseHTML(botHtml));
+  });
+  event.preventDefault();
+
+}
+
+let allButtons = document.querySelectorAll('div.side-body > button');
+let btnFuncs = [Localize, Municipalize, Nationalize, Globalize, Economize, Ecologize]
+
+for (let i = 0; i < allButtons.length; i++) {
+  allButtons[i].addEventListener('click', btnFuncs[i])
+}
 
 
 
